ebed854e54fd24fbfc74f9aac623dbff10229bb5
loryncien <lluocheng88@gmail.com>
2023-03-05 12:04:32 +0800

    Update diy-lean.sh

--- scripts/diy-lean.sh
+++ scripts/diy-lean.sh
@@ -1,6 +1,8 @@
+echo 'diy-lean.sh ...'
 # Modify localtime in Homepage
-echo 'Modify localtime in Homepage...'
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/x86/index.htm
+# Shows increased compile time
+#sed -i "s/<%=pcdata(ver.distversion)%>/& (By @Cheng build $(TZ=UTC-8 date "+%Y-%m-%d"))/g" package/lean/autocore/files/x86/index.htm
 # Modify hostname in Homepage
 sed -i 's/${g}'"'"' - '"'"'//g' package/lean/autocore/files/x86/autocore
 
@@ -8,6 +10,6 @@ pushd package/lean/default-settings/files
 # 设置密码为空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' zzz-default-settings
 # 版本号里显示一个自己的名字
-export date_version=$(TZ=UTC-8 date +'%Y.%m.%d')
+export date_version=$(TZ=UTC-8 date +'%Y-%m-%d')
 sed -ri "s#(R[0-9].*[0-9])#\1 (By @Cheng build ${date_version}) #g" zzz-default-settings
 popd
